66084efca96e4c5d64d817464a8ebf7ca082681b
Yaguang Tang <heut2008@gmail.com>
2022-11-19 23:03:04 +0800

    Update truffle-config.js
    add Polygon test network

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,14 @@ module.exports = {
       port: 7545,
       network_id: '*', // Match any network id
     },
+     matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      host: 'https://rpc-mumbai.matic.today',
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     rinkeby: {
       host: '127.0.0.1',
       port: 8545,
